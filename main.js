25bcb1bd8f4b1c476702c22a3298823a8ea6a277
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-08-03 16:08:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+length1 = 0;
+text = "Write Your Costomised 'Text' In The Below Input";
+
 function setup(){
     canvas = createCanvas(300,300);
     canvas.position(100,100);
@@ -12,5 +15,21 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length != 0){
         console.log(results);
+        length1 = floor(results[0].pose.rightWrist.x - results[0].pose.leftWrist.x);
+        document.getElementById("wht").innerHTML = "Font Size of Your Text is "+length1;
+    }
+}
+function draw(){
+    testSize(length1);
+    fill("white");
+    text(text,50,50);
+}
+input = document.getElementById("input")
+input.addEventListener("keydown",yes)
+
+function yes(e){
+    if(e.key == "Enter"){
+        text = input.value;
+        input.value = "";
     }
 }
