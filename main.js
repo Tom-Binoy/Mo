05b06ba58c0b5612dce5656c8f521a59733e9645
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-08-08 20:16:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ length1 = 0;
 word = "Write Your Costomised 'Text' In The Below Input";
 
 function setup(){
-    canvas = createCanvas(300,600);
+    canvas = createCanvas(500,650);
     canvas.position(100,100);
     canvas.center();
     
@@ -25,7 +25,7 @@ function draw(){
     background("rgba(255, 20, 145, 0.807)");
     textSize(length1);
     fill("white");
-    text(word,50,50);
+    text(word,10,250);
 }
 input = document.getElementById("input")
 input.addEventListener("keydown",yes)
